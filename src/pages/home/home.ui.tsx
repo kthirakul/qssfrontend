4214a5b115caea18bd85727d7dcba30126ebf56a
Kwang Thibamrung <kwangthirakul@gmail.com>
2022-04-02 22:21:21 +0700

    setup font sarabun

--- src/pages/home/home.ui.tsx
+++ src/pages/home/home.ui.tsx
@@ -1,3 +1,4 @@
+import logoUru from '../../assets/images/uru-logo.png'
 import styles from './home.module.css'
 import HomeUse from './home.use'
 
@@ -5,7 +6,7 @@ const HomeUi: React.FC = () => {
   const { number, route } = HomeUse()
   return (
     <div className={styles['app']}>
-      <div>Route : {route.val.route}</div>
+      {/* <div>Route : {route.val.route}</div>
       <div>{number.val.count}</div>
       <button
         className={`${styles['btn']} ${styles['btn-blue']}`}
@@ -18,7 +19,14 @@ const HomeUi: React.FC = () => {
         onClick={() => number.set({ type: 'decrement' })}
       >
         decrement number
-      </button>
+      </button> */}
+      <div className='flex items-center'>
+        <img src={logoUru} alt='' />
+        <div className='text-3xl font-bold'>
+          <div>URU</div>
+          <div>EQS System</div>
+        </div>
+      </div>
     </div>
   )
 }
