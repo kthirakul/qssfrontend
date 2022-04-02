1e4cc1bf021dc1295bcc8ce72f5af4b213e3d99e
Kwang Thibamrung <kwangthirakul@gmail.com>
2022-04-03 02:33:34 +0700

    add home ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import axios from 'axios'
-import { useEffect } from 'react'
 import styles from './app.module.css'
 import AppRoute from './app.route'
 function App() {
-  const a = async () => await axios.get('http://localhost:3001/')
-
-  useEffect(() => {
-    const b = async () => {
-      console.log('App.js | await a()| = ', (await a()).data)
-    }
-    b()
-  }, [])
   return (
     <div className={styles['app']}>
       <AppRoute />
@@ -19,3 +9,12 @@ function App() {
 }
 
 export default App
+
+// const a = async () => await axios.get('http://localhost:3001/')
+
+// useEffect(() => {
+//   const b = async () => {
+//     console.log('App.js | await a()| = ', (await a()).data)
+//   }
+//   b()
+// }, [])
--- src/pages/home/home.ui.tsx
+++ src/pages/home/home.ui.tsx
@@ -1,3 +1,4 @@
+import Wallpaper from '../../assets/images/logo-wallpaper.png'
 import logoUru from '../../assets/images/uru-logo.png'
 import styles from './home.module.css'
 import HomeUse from './home.use'
@@ -5,26 +6,36 @@ import HomeUse from './home.use'
 const HomeUi: React.FC = () => {
   const { number, route } = HomeUse()
   return (
-    <div className={styles['app']}>
-      {/* <div>Route : {route.val.route}</div>
-      <div>{number.val.count}</div>
-      <button
-        className={`${styles['btn']} ${styles['btn-blue']}`}
-        onClick={() => number.set({ type: 'increment' })}
-      >
-        increment number
-      </button>
-      <button
-        className={`${styles['btn']} ${styles['btn-blue']}`}
-        onClick={() => number.set({ type: 'decrement' })}
-      >
-        decrement number
-      </button> */}
-      <div className='flex items-center'>
+    <div className={styles['w-home']}>
+      {/* wallpaper */}
+      <div className={styles['w-wallpaper']}>
+        <img src={Wallpaper} alt='' />
+      </div>
+
+      {/* logo */}
+      <div className={styles['w-logo']}>
         <img src={logoUru} alt='' />
-        <div className='text-3xl font-bold'>
+        <div className='text-3xl font-bold text-slate-900'>
           <div>URU</div>
           <div>EQS System</div>
+        </div>
+      </div>
+
+      {/* content */}
+      <div className='flex w-full h-full'>
+        <div className='w-full flex flex-col gap-y-3 items-center pt-12'>
+          <div className={styles['t-org']}>สำหรับหน่วยงานภายนอก</div>
+          <button className={styles['btn-org']}>ขอตรวจสอบวุฒิการศึกษา</button>
+          <button className={styles['btn-org']}>ดูสถานะคำขอตรวจสอบวุฒิ</button>
+        </div>
+
+        <div className='w-full'>
+          <div className={styles['w-emp']}>
+            <div className='text-2xl font-semibold text-slate-800'>บุคลากร</div>
+            <input className={styles['t-input']} type='text' placeholder='อีเมล' />
+            <input className={styles['t-input']} type='text' placeholder='รหัสผ่าน' />
+            <button className={styles['btn-login']}>เข้าสู่ระบบ</button>
+          </div>
         </div>
       </div>
     </div>
@@ -32,3 +43,18 @@ const HomeUi: React.FC = () => {
 }
 
 export default HomeUi
+
+//  <div>Route : {route.val.route}</div>
+//       <div>{number.val.count}</div>
+//       <button
+//         className={`${styles['btn']} ${styles['btn-blue']}`}
+//         onClick={() => number.set({ type: 'increment' })}
+//       >
+//         increment number
+//       </button>
+//       <button
+//         className={`${styles['btn']} ${styles['btn-blue']}`}
+//         onClick={() => number.set({ type: 'decrement' })}
+//       >
+//         decrement number
+//       </button>
